52cbbfc6e8f1ad137de86b17720747a290972697
Andrea Vecchi <andreavecchi92@protonmail.com>
2025-02-27 19:13:03 +0100

    aggiunto l'esercizio 1

--- Chapter_17/esercizio 1.py
+++ Chapter_17/esercizio 1.py
@@ -1,3 +1,36 @@
-from random import randint
+import requests
+import plotly.express as px
 
-print(randint(1,10))
+url="https://api.github.com/search/repositories"
+url+=("?q=language:haskell+sort:stars+stars:>10000")
+
+intestazioni={"Accept":"application/vnd.github.v3+json"}
+r=requests.get(url,headers=intestazioni)
+print(f"Status code: {r.status_code}")
+
+response_dict=r.json()
+print(f"Complete results: {not response_dict['incomplete_results']}")
+
+repo_dicts=response_dict['items']
+
+repo_links,stars,hover_texts=[],[],[]
+for repo_dict in repo_dicts:
+    repo_name=(repo_dict['name'])
+    repo_url=(repo_dict['html_url'])
+    repo_link=f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+    stars.append(repo_dict['stargazers_count'])
+    owner=repo_dict['owner']['login']
+    description=repo_dict['description']
+    hover_text=f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
+
+title="Le repo github con più stars"
+labels={"x":"Nome del progetto","y":"Stelle assegnate"}
+
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts, height=600)
+fig.update_layout(title_font_size=28,xaxis_title_font_size=20,yaxis_title_font_size=20)
+fig.update_traces(marker_color="SteelBlue", marker_opacity=0.6)
+
+fig.show()
